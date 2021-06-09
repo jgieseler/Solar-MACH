ab52b35aa1b77e10d94234956692721ab96f774c
jgieseler <jan.gieseler@utu.fi>
2021-06-09 15:26:44 +0300

    sidebar

--- flare_sketch.py
+++ flare_sketch.py
@@ -11,12 +11,12 @@ d = st.sidebar.date_input("Select date", datetime.date.today())
 t = st.sidebar.time_input('Select time', datetime.time(16, 45))
 # st.write('Selected time:', t)
 date = datetime.datetime.combine(d, t).strftime("%Y-%m-%d %H:%M:%S")
-st.write('Selected datetime:', date)
+# st.write('Selected datetime:', date)
 
 # st.sidebar.markdown("""---""")
 
-st.header('Chose bodies/spacecraft and measured solar wind speeds')
-st.subheader('vst_list: leave empty for nominal speed of vsw=400 km/s')
+st.sidebar.subheader('Chose bodies/spacecraft and measured solar wind speeds')
+st.sidebar.subheader('vst_list: leave empty for nominal speed of vsw=400 km/s')
 st.write(print_body_list())
 
 body_list = ['STEREO-A', 'STEREO-B', 'Earth', 'MPO', 'PSP', 'Solar Orbiter',
@@ -27,16 +27,16 @@ vsw_list = [300, 400, 500, 600, 700, 800, 900, 200]
 # vsw_list = st.multiselect('v', [300, 400, 500, 600, 700, 800, 900, 200])
 # st.write(body_list, vsw_list)
 
-st.markdown("""---""")
+# st.markdown("""---""")
 
 st.sidebar.subheader('Provide a reference longitude in Carrington coordinates (e.g. flare longitude)')
 reference_long = 20
 reference_lat = -20
 reference_long = st.sidebar.slider('Reference longitude:', 0, 360, 20)
 reference_lat = st.sidebar.slider('Reference latitude:', -180, 180, -20)
-st.write('Selected reference longitude and latituide:', reference_long, reference_lat)
+# st.write('Selected reference longitude and latituide:', reference_long, reference_lat)
 
-st.sidebar.markdown("""---""")
+# st.sidebar.markdown("""---""")
 
 # Initialize the Bodies
 c = HeliosphericConstellation(date, body_list, vsw_list, reference_long,
