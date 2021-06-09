6b25d31f158c0063562b173061d7237600dcd013
jgieseler <jan.gieseler@utu.fi>
2021-06-09 16:22:13 +0300

    clean-up

--- flare_sketch.py
+++ flare_sketch.py
@@ -5,44 +5,35 @@ import datetime
 st.title('Multi-spacecraft longitudinal configuration plotter')
 
 # st.sidebar.subheader('Provide date and time')
-# date = '2020-05-01 13:00:00'
 d = st.sidebar.date_input("Select date", datetime.date.today())
-# st.write('Selected date:', d)
 t = st.sidebar.time_input('Select time', datetime.time(16, 45))
-# st.write('Selected time:', t)
 date = datetime.datetime.combine(d, t).strftime("%Y-%m-%d %H:%M:%S")
-# st.write('Selected datetime:', date)
 
-# st.sidebar.markdown("""---""")
 
-# col1, col2 = st.beta_columns([1, 2])
-
-st.sidebar.subheader('Provide a reference longitude in Carrington coordinates (e.g. flare longitude)')
-reference_long = 20
-reference_lat = -20
+st.sidebar.subheader('Provide a reference longitude in Carrington coordinates\
+                     (e.g. flare longitude)')
 reference_long = st.sidebar.slider('Reference longitude:', 0, 360, 20)
 reference_lat = st.sidebar.slider('Reference latitude:', -180, 180, -20)
-# st.write('Selected reference longitude and latituide:', reference_long, reference_lat)
+# st.write('Selected reference longitude and latituide:', reference_long,
+#           reference_lat)
 
 
 st.sidebar.subheader('Chose bodies/spacecraft and measured solar wind speeds')
-# st.sidebar.subheader('vst_list: leave empty for nominal speed of vsw=400 km/s')
-
-# body_list = ['STEREO-A', 'STEREO-B', 'Earth', 'MPO', 'PSP', 'Solar Orbiter', 'Mars']
-# vsw_list = [300, 400, 500, 600, 700, 800, 900, 200]
-
-full_body_list = st.sidebar.text_area('bodies/spacecraft (scroll down for full list)', "STEREO-A, STEREO-B, Earth, MPO, PSP, Solar Orbiter, Mars", height=1)
-vsw_list = st.sidebar.text_area('vst_list', "400, 400, 400, 400, 400, 400, 400, 400", height=None)
+# st.sidebar.subheader('vst_list: leave empty for nominal speed of \
+#                       vsw=400 km/s')
+full_body_list = \
+    st.sidebar.text_area('bodies/spacecraft (scroll down for full list)',
+                         'STEREO-A, STEREO-B, Earth, MPO, PSP, Solar Orbiter,\
+                          Mars', height=50)
+vsw_list = st.sidebar.text_area('vst_list', '400, 400, 400, 400, 400, 400, \
+                                400, 400', height=50)
 body_list = full_body_list.split(',')
 vsw_list = vsw_list.split(',')
-for i in range(len(body_list)):
-    body_list[i] = body_list[i].lstrip()
+body_list = [np.int(body_list[i].lstrip()) for i in range(len(body_list))]
 vsw_list = [np.int(vsw_list[i].lstrip()) for i in range(len(vsw_list))]
 
 all_bodies = print_body_list()
 st.sidebar.table(all_bodies)
-
-# st.markdown("""---""")
 
 
 # Initialize the Bodies
