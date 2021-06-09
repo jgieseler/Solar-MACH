42f0006b27902694c1c85e41323a64ec1471baf4
jgieseler <jan.gieseler@utu.fi>
2021-06-09 12:03:50 +0300

    streamlit-ing

--- flare_sketch.py
+++ flare_sketch.py
@@ -2,24 +2,25 @@ import streamlit as st
 from backmapping import *
 import datetime
 
-# provide date and time
-date = '2020-05-01 13:00:00'
+
+st.write('provide date and time')
+# date = '2020-05-01 13:00:00'
 d = st.date_input("Select date", datetime.date.today())
 st.write('Selected date:', d)
 t = st.time_input('Select time', datetime.time(16, 45))
 st.write('Selected time:', t)
+date = datetime.datetime.combine(d, t)
+st.write('Selected datetime:', date)
 
 
-# Chose bodies/spacecraft and measured solar wind speeds
-# (vst_list: leave empty for nominal speed of vsw=400 km/s)
+st.write('Chose bodies/spacecraft and measured solar wind speeds (vst_list: leave empty for nominal speed of vsw=400 km/s)')
 st.write(print_body_list())
 
 body_list = ['STEREO-A', 'STEREO-B', 'Earth', 'MPO', 'PSP', 'Solar Orbiter',
              'Mars']
 vsw_list = [300, 400, 500, 600, 700, 800, 900, 200]
 
-# Provide a reference longitude in Carrington coordinates (e.g. flare
-# longitude)
+st.write('Provide a reference longitude in Carrington coordinates (e.g. flare longitude)')
 reference_long = 20
 reference_lat = -20
 
