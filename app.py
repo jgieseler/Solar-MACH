efe07dcbea1747c1155aee05f79393e2fc0ed3f0
jgieseler <jan.gieseler@utu.fi>
2021-06-10 16:51:14 +0300

    plot option for reference longitude

--- app.py
+++ app.py
@@ -9,42 +9,54 @@ st.set_page_config(page_title='Solar-MACH', page_icon=":satellite:",
 st.title('Multi-spacecraft longitudinal configuration plotter')
 
 # st.sidebar.subheader('Provide date and time')
-d = st.sidebar.date_input("Select date", datetime.date.today())
-t = st.sidebar.time_input('Select time', datetime.time(13, 0))
-date = datetime.datetime.combine(d, t).strftime("%Y-%m-%d %H:%M:%S")
+with st.sidebar.beta_container():
+    d = st.sidebar.date_input("Select date", datetime.date.today())
+    t = st.sidebar.time_input('Select time', datetime.time(13, 0))
+    date = datetime.datetime.combine(d, t).strftime("%Y-%m-%d %H:%M:%S")
 
 # plotting settings
-st.sidebar.subheader('Plot options:')
-plot_spirals = st.sidebar.checkbox('Parker spiral for each body', value=True)
-plot_sun_body_line = st.sidebar.checkbox('Straight line from Sun to body', value=True)
-show_earth_centered_coord = st.sidebar.checkbox('Add Earth-centered coord. system', value=False)
+with st.sidebar.beta_container():
+    st.sidebar.subheader('Plot options:')
+    plot_spirals = st.sidebar.checkbox('Parker spiral for each body', value=True)
+    plot_sun_body_line = st.sidebar.checkbox('Straight line from Sun to body', value=True)
+    show_earth_centered_coord = st.sidebar.checkbox('Add Earth-centered coord. system', value=False)
 
-st.sidebar.subheader('Reference longitude in Carrington coordinates (e.g. flare longitude)')
-reference_long = st.sidebar.slider('Reference longitude:', 0, 360, 20)
-reference_lat = st.sidebar.slider('Reference latitude:', -180, 180, -20)
-# st.write('Selected reference longitude and latituide:',
-#          reference_long, reference_lat)
+    plot_reference = st.sidebar.checkbox('Plot reference (e.g. flare)', value=True)
+
+    with st.sidebar.beta_expander("Reference longitude in Carrington coordinates (e.g. flare longitude)", expanded=plot_reference):
+        # st.sidebar.subheader('Reference longitude in Carrington coordinates (e.g. flare longitude)')
+        reference_long = st.slider('Reference longitude:', 0, 360, 20)
+        reference_lat = st.slider('Reference latitude:', -180, 180, -20)
+    
+    if plot_reference is False:
+        reference_long = None
+        reference_lat = None
+
+
+    # st.write('Selected reference longitude and latituide:',
+    #          reference_long, reference_lat)
 
 
 st.sidebar.subheader('Choose bodies/spacecraft and measured solar wind speeds')
-# st.sidebar.subheader('vsw_list: leave empty for nominal speed of \
-#                       vsw=400 km/s')
-full_body_list = \
-    st.sidebar.text_area('Bodies/spacecraft (scroll down for full list)',
-                         'STEREO-A, STEREO-B, Earth, MPO, PSP, Solar Orbiter, Mars',
-                         height=50)
-vsw_list = \
-    st.sidebar.text_area('Solar wind speed per body/SC (mind the order!)', '400, 400, 400, 400, 400, 400, 400',
-                         height=50)
-body_list = full_body_list.split(',')
-vsw_list = vsw_list.split(',')
-body_list = [body_list[i].lstrip() for i in range(len(body_list))]
-vsw_list = [np.int(vsw_list[i].lstrip()) for i in range(len(vsw_list))]
+with st.sidebar.beta_container():
+    # st.sidebar.subheader('vsw_list: leave empty for nominal speed of \
+    #                       vsw=400 km/s')
+    full_body_list = \
+        st.sidebar.text_area('Bodies/spacecraft (scroll down for full list)',
+                            'STEREO-A, STEREO-B, Earth, MPO, PSP, Solar Orbiter, Mars',
+                            height=50)
+    vsw_list = \
+        st.sidebar.text_area('Solar wind speed per body/SC (mind the order!)', '400, 400, 400, 400, 400, 400, 400',
+                            height=50)
+    body_list = full_body_list.split(',')
+    vsw_list = vsw_list.split(',')
+    body_list = [body_list[i].lstrip() for i in range(len(body_list))]
+    vsw_list = [int(vsw_list[i].lstrip()) for i in range(len(vsw_list))]
 
-all_bodies = print_body_list()
-st.sidebar.table(all_bodies)
+    all_bodies = print_body_list()
+    st.sidebar.table(all_bodies)
 
-st.sidebar.markdown('[Complete list of available bodies](https://ssd.jpl.nasa.gov/horizons.cgi?s_target=1#top)')
+    st.sidebar.markdown('[Complete list of available bodies](https://ssd.jpl.nasa.gov/horizons.cgi?s_target=1#top)')
 
 
 # Initialize the Bodies
