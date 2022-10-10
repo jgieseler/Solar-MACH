890aaaaa032f6fb70e5234398653389401b5ee93
Jan Gieseler <39489154+jgieseler@users.noreply.github.com>
2022-10-10 16:27:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -419,8 +419,7 @@ st.markdown('[The version used here](https://github.com/jgieseler/Solar-MACH) is
              [![Get in contact](https://img.shields.io/static/v1?label=Contact&message=jan.gieseler@utu.fi&color=red&logo=gmail)](mailto:jan.gieseler@utu.fi?subject=Solar-MACH)')
 
 st.markdown('For the streamlit-independent python package of Solar-MACH that runs in a python shell or Jupyter Notebook, \
-             refer to [**solarmach**](https://github.com/jgieseler/solarmach):&nbsp;\
-             [<img src="https://img.shields.io/static/v1?label=GitHub&message=solarmach&color=blue&logo=github" height="20">](https://github.com/jgieseler/solarmach/) \ 
+             refer to [**solarmach**](https://github.com/jgieseler/solarmach):&nbsp;\ 
              [<img src="https://img.shields.io/pypi/v/solarmach?style=flat&logo=pypi" height="20">](https://pypi.org/project/solarmach/) \
              [<img src="https://img.shields.io/conda/vn/conda-forge/solarmach?style=flat&logo=anaconda" height="20">](https://anaconda.org/conda-forge/solarmach/)', unsafe_allow_html=True)
 
